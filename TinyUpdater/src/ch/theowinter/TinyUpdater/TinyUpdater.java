278fd2cbae0481c303c128a8f9bb7122f28eb1b7
aerobless <theo.winter@gmail.com>
2014-04-05 16:32:03 +0200

    TinyUpdater inital commit

---
+++ TinyUpdater/src/ch/theowinter/TinyUpdater/TinyUpdater.java
@@ -0,0 +1,85 @@
+package ch.theowinter.TinyUpdater;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class TinyUpdater {
+	private static String updateURL;
+	private static int waitTime;
+
+	/*
+	 * We expect args in the form of a waitTime-Count (seconds) and one http download-URL 
+	 * that ends with a ".jar" file. This jar file has the same name as the file
+	 * we're supposed to be updating.
+	 * TinyUpdater waits until his waitTime is over and then tries to download and write the file.
+	 * The original application needs to be shutdown at that point otherwise the update fails.
+	 */
+	public static void main(String[] args) {
+		System.out.println("  TinyUpdater");
+		System.out.println("<<----------->>");
+		System.out.println("initalizing");
+		updateURL = args[1];
+		System.out.print(".");
+		waitTime = Integer.parseInt(args[0]);
+		System.out.print(".");
+		String[] updateArray  = updateURL.split("/");
+		String downloadPath = getJarDirectory(updateArray[updateArray.length-1]);
+		System.out.print(".");
+		System.out.println("Preparing for update");
+		for(int i = 0; i<waitTime; i++){
+			try {
+				Thread.sleep(i*100);
+				System.out.print(".");
+			} catch (InterruptedException e) {
+				System.out.println("Error - Can't sleep properly.");
+			}
+		}
+		System.out.println("Downloading update..");
+		downloadFile(updateURL, downloadPath);
+		System.out.print(".....");
+		System.out.println("Update complete..");
+		try {
+			Runtime.getRuntime().exec("java -jar "+downloadPath);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void downloadFile(String downloadURL, String filenameAndPath) {
+		try {
+			URL url = new URL(downloadURL);
+			URLConnection con = url.openConnection(); 
+			DataInputStream dis = new DataInputStream(con.getInputStream());
+			byte[] fileData = new byte[con.getContentLength()];
+			for (int x = 0; x < fileData.length; x++) {
+				fileData[x] = dis.readByte();
+			}
+			dis.close(); 
+			FileOutputStream fos = new FileOutputStream(new File(filenameAndPath));
+			fos.write(fileData); 
+			fos.close();
+		} catch (MalformedURLException m) {
+			System.out.println(m);
+		} catch (IOException io) {
+			System.out.println(io);
+		}
+	}
+	
+	public static String getJarDirectory(String filename){
+		URL jarLocation = TinyUpdater.class.getProtectionDomain().getCodeSource().getLocation();
+		URL dataXML = null;
+		try {
+			dataXML = new URL(jarLocation, filename);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dataXML.getPath();
+	}
+}
